3af38a8f97de1cafc77cbc3d47b545517a46a908
Andrey Bondar <andrey@bondar.ru>
2016-11-01 15:42:03 +0300

    Mirror reports (#3)
    * Mongo data persistence
    
    * Mirror summary and dynamic report
    
    * Fixed raising on validation
    
    * Client manager authentication, reports for manager
    
    * Serializers tests

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,3 +1,9 @@
-class ValidationError(Exception):
+class APIException(Exception):
+    status_code = 400
+
     def __init__(self, message):
         self.message = message
+
+
+class ValidationError(APIException):
+    status_code = 400
--- app/views.py
+++ app/views.py
@@ -1,21 +1,40 @@
 import asyncio
+import collections
 from aiohttp import web
 
 from app.events.manager import get_events
-from app.exceptions import ValidationError
+from app.exceptions import ValidationError, APIException
 from app.serializers import DateRangeSerializer
 
 
-class LoginRequiredView(web.View):
+class BaseView(web.View):
+    @asyncio.coroutine
+    def __iter__(self):
+        try:
+            response = yield from super(BaseView, self).__iter__()
+        except APIException as e:
+            if isinstance(e.message, str):
+                message = {'error': e.message}
+            elif isinstance(e.message, collections.Mapping) or isinstance(e.message, collections.Sequence):
+                message = e.message
+            else:
+                message = "Can't serialize {} to dict or list".format(type(e.message).__name__)
+                raise TypeError(message)
+            return web.json_response(message, status=e.status_code)
+        return response
+
+
+class LoginRequiredView(BaseView):
     authentication_class = None
 
     @asyncio.coroutine
     def __iter__(self):
-        authenticated = yield from self.authentication_class(self.request).authenticate()
-        if not authenticated:
-            return web.json_response({
-                'error': 'invalid credentials provided'
-            }, status=401)
+        if self.request.method.lower() == 'get':
+            authenticated = yield from self.authentication_class(self.request).authenticate()
+            if not authenticated:
+                return web.json_response({
+                    'error': 'invalid credentials provided'
+                }, status=401)
         response = yield from super(LoginRequiredView, self).__iter__()
         return response
 
@@ -31,4 +50,3 @@ class EventListView(web.View):
         date_to = serializer.validated_data.get('date_to')
         events = await get_events(date_from, date_to, self.request['cache'])
         return web.json_response(events)
-
